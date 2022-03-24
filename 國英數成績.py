76d3a520a6bb2c19b87e589e924c723b54c092bf
Eliotz21 <95619638+Eliotz21@users.noreply.github.com>
2022-03-24 20:07:58 +0800

    國英數成績

---
+++ 國英數成績.py
@@ -0,0 +1,9 @@
+ch = float(input('請輸入國文成績:'))
+en = float(input('請輸入英文成績:'))
+ma = float(input('請輸入數學成績:'))
+
+score_totle = ch + en + ma
+score_avg = score_totle/3
+
+print('總成績為:', score_totle)
+print('平均成績為:', score_avg)
